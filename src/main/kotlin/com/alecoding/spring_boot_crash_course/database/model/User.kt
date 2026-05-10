bc25f5b05b196a3d1b2e2779fd03e9fde0b9dcaa
Ale Fernandes <alexandre.fernandes@campspot.com>
2026-05-10 09:59:15 -0300

    feat(security): jwtTokenSecret defined

--- src/main/kotlin/com/alecoding/spring_boot_crash_course/database/model/User.kt
+++ src/main/kotlin/com/alecoding/spring_boot_crash_course/database/model/User.kt
@@ -1,4 +1,14 @@
 package com.alecoding.spring_boot_crash_course.database.model
 
-class User {
-}
+import jakarta.validation.constraints.Email
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.Instant
+
+@Document("users")
+class User (
+    val email: String,
+    val hashedPassword: String,
+    @Id val id: ObjectId
+)
